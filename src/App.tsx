2d7700944527314c976c68eee5a22635573de1f9
Frida Jacobsson <fridahelenajacobsson@gmail.com>
2023-01-22 12:14:10 +0100

    Create basic files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Our new webshop</h1>
+      </header>
+      <div className="Main-content"></div>
+    </div>
+  );
+}
+
+export default App;
